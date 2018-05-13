547e9ae1cfb05ca5cc0c9162d35cfb5db25f2821
Keith Bennett <keithrbennett@gmail.com>
2018-05-13 10:50:22 -0400

    Add report_string, name_for_id.

--- lib/primordial_books/chart_of_accounts.rb
+++ lib/primordial_books/chart_of_accounts.rb
@@ -1,30 +1,52 @@
 module PrimordialBooks
-  class ChartOfAccounts
+class ChartOfAccounts
 
-    class Account < Struct.new(:id, :name); end
+  class Account < Struct.new(:id, :name); end
 
-    attr_reader :date_prefix, :title, :accounts
+  attr_reader :date_prefix, :title, :accounts
 
-    def initialize(input_string)
-      @accounts = []
-      lines = input_string.split("\n")
-      lines.each do |line|
-        puts line
-        case line.strip
-        when /^@title:/
-          @title = line.split('title:').last.strip
-        when /^@date_prefix:/
-          puts 'found date prefix'
-          @date_prefix = line.split('@date_prefix:').last.strip
-        when /^$/
-          # ignore empty line
-        else
-          # this is an account line in the form: 101 blah blah blah
-          matcher = line.match(/^(\S+)\s+(.*)$/)
-          @accounts << Account.new(matcher[1], matcher[2])
-        end
-      end
+
+  def initialize(input_string)
+    @accounts = []
+    lines = input_string.split("\n")
+    lines.each { |line| parse_line(line) }
+  end
+
+
+  def parse_line(line)
+    case line.strip
+    when /^@title:/
+      @title = line.split('title:').last.strip
+    when /^@date_prefix:/
+      @date_prefix = line.split('@date_prefix:').last.strip
+    when /^$/
+      # ignore empty line
+    else
+      # this is an account line in the form: 101 blah blah blah
+      matcher = line.match(/^(\S+)\s+(.*)$/)
+      accounts << Account.new(matcher[1], matcher[2])
+    end
+  end
+
+
+  def report_string
+    result = ''
+
+    if title
+      result << title << "\n\n"
     end
 
+    id_width = @accounts.inject(0) { |width, a| width = [width, a.id.length].max }
+    format_string = "%#{id_width}s  %s\n"
+    accounts.each { |a| result << format_string % [a.id, a.name] }
+
+    result
   end
+
+
+  def name_for_id(id)
+    found = accounts.detect { |a| a.id == id }
+    found ? found.name : nil
+  end
+end
 end
--- spec/chart_of_accounts_spec.rb
+++ spec/chart_of_accounts_spec.rb
@@ -20,8 +20,18 @@ RSpec.describe PrimordialBooks::ChartOfAccounts do
     data = '101 My Bank Checking Account'
     expect(ChartOfAccounts.new(data).accounts).to eq([ChartOfAccounts::Account.new('101', 'My Bank Checking Account')])
   end
+
+  it 'can produce a report string' do
+    data = "@title: Title\n101 Cash in Bank\n"
+    report_string = ChartOfAccounts.new(data).report_string
+    expect(report_string).to include('Title')
+    expect(report_string).to include('101')
+    expect(report_string).to include('Cash in Bank')
+  end
+
+  it 'can look up a name by an id' do
+    data = "101 Cash in Bank\n201 Loan Payable\n301 Retained Earnings"
+    expect(ChartOfAccounts.new(data).name_for_id('201')).to eq('Loan Payable')
+  end
 end
-
-
-
 end
