c632318f729b720eda5e179feb5ad30acd5ccea5
Keith Bennett <keithrbennett@gmail.com>
2018-05-13 02:09:34 -0400

    Basic Chart of Accounts done.

--- lib/primordial_books/chart_of_accounts.rb
+++ lib/primordial_books/chart_of_accounts.rb
@@ -3,16 +3,25 @@ module PrimordialBooks
 
     class Account < Struct.new(:id, :name); end
 
-    attr_reader :title
+    attr_reader :date_prefix, :title, :accounts
 
     def initialize(input_string)
+      @accounts = []
       lines = input_string.split("\n")
       lines.each do |line|
         puts line
-        case line
+        case line.strip
         when /^@title:/
-          $stderr.puts "\n\nfound title line"; $stderr.puts line
           @title = line.split('title:').last.strip
+        when /^@date_prefix:/
+          puts 'found date prefix'
+          @date_prefix = line.split('@date_prefix:').last.strip
+        when /^$/
+          # ignore empty line
+        else
+          # this is an account line in the form: 101 blah blah blah
+          matcher = line.match(/^(\S+)\s+(.*)$/)
+          @accounts << Account.new(matcher[1], matcher[2])
         end
       end
     end
--- spec/chart_of_accounts_spec.rb
+++ spec/chart_of_accounts_spec.rb
@@ -10,6 +10,16 @@ RSpec.describe PrimordialBooks::ChartOfAccounts do
     data = '@title: My Chart of Accounts'
     expect(ChartOfAccounts.new(data).title).to eq('My Chart of Accounts')
   end
+
+  it 'can read its date prefix' do
+    data = '@date_prefix: 2018-'
+    expect(ChartOfAccounts.new(data).date_prefix).to eq('2018-')
+  end
+
+  it 'can read an account code and name' do
+    data = '101 My Bank Checking Account'
+    expect(ChartOfAccounts.new(data).accounts).to eq([ChartOfAccounts::Account.new('101', 'My Bank Checking Account')])
+  end
 end
 
 
