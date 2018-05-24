5e6811c81682cc695cf72f362409b2a6388050e5
Keith Bennett <keithrbennett@gmail.com>
2018-05-24 08:39:23 -0400

    Fix AccountNotFoundError super call.

--- lib/rock_books/account_not_found_error.rb
+++ lib/rock_books/account_not_found_error.rb
@@ -4,13 +4,17 @@ class AccountNotFoundError < RuntimeError
   attr_accessor :bad_account_code, :document_name, :line
 
   def initialize(bad_account_code, document_name = nil, line = nil)
-    super
     self.bad_account_code = bad_account_code
+    super(to_s)
   end
 
 
   def to_s
-    "Account code not found in chart of accounts: #{bad_account_code}, document: #{document_name}, line: #{line}"
+    s = "Account code not found in chart of accounts: #{bad_account_code}"
+    if document_name && line
+      s << ", document: #{document_name}, line: #{line}"
+    end
+    s
   end
 end
 end
