394fb1896fa6b18e05ed5862b7db7f7d477c5fd0
Keith Bennett <keithrbennett@gmail.com>
2018-05-28 20:32:11 -0400

    Add total of assets, liabilities, and equity to balance sheet.

--- lib/rock_books/reports/balance_sheet.rb
+++ lib/rock_books/reports/balance_sheet.rb
@@ -25,14 +25,17 @@ class BalanceSheet < Struct.new(:chart_of_accounts, :journals, :end_date, :page_
 
 
   def process_section(totals, section_type)
+    section_total_amount = 0
     account_codes_this_section = chart_of_accounts.account_codes_of_type(section_type)
     totals_this_section = totals.select do |account_code, amount|
       account_codes_this_section.include?(account_code)
     end
 
+    section_total_amount = totals_this_section.map { |aa| aa.last }.sum
+
     output = "\n\nAccount Type: #{section_type}\n"
     output << generate_and_format_totals(totals_this_section, chart_of_accounts)
-    output
+    [ output, section_total_amount ]
   end
 
 
@@ -42,9 +45,13 @@ class BalanceSheet < Struct.new(:chart_of_accounts, :journals, :end_date, :page_
     acct_amounts = acct_amounts_in_documents(journals, filter)
     totals = AcctAmount.aggregate_amounts_by_account(acct_amounts)
     output = format_header
+    total_amount = 0
     %i(asset  liability  owners_equity).each do |section_type|
-      output << process_section(totals, section_type) << "\n\n"
+      section_output, section_total_amount = process_section(totals, section_type)
+      output << section_output << "\n\n"
+      total_amount += section_total_amount
     end
+    output << "\n#{"%12.2f    Sum of Assets, Liabilities, and Equity" % total_amount}\n============\n"
     output
   end
 
