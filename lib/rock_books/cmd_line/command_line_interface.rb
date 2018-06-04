6016eb3fc89e0bdadf0a615519a01607cf2058a7
Keith Bennett <keithrbennett@gmail.com>
2018-06-04 11:43:10 -0400

    Change receipts options to all, missing, existing, and include only receipt filespec, not doc_short_name. Fix whitespace.

--- lib/rock_books/cmd_line/command_line_interface.rb
+++ lib/rock_books/cmd_line/command_line_interface.rb
@@ -7,37 +7,37 @@ require_relative '../helpers/book_set_loader'
 
 module RockBooks
 
-  class CommandLineInterface
+class CommandLineInterface
 
-    # Enable users to type methods of this class more conveniently:
-    include JournalEntryFilters
-    extend Forwardable
+  # Enable users to type methods of this class more conveniently:
+  include JournalEntryFilters
+  extend Forwardable
 
-    attr_reader :book_set, :interactive_mode, :run_options, :verbose_mode
-
-
-    class Command < Struct.new(:min_string, :max_string, :action); end
+  attr_reader :book_set, :interactive_mode, :run_options, :verbose_mode
 
 
-    class BadCommandError < RuntimeError; end
+  class Command < Struct.new(:min_string, :max_string, :action); end
 
 
-    # Enable use of some BookSet methods in shell with long and short names aaa, ae:
+  class BadCommandError < RuntimeError; end
 
-    def_delegator :book_set, :all_acct_amounts
-    def_delegator :book_set, :all_acct_amounts, :aaa
 
-    def_delegator :book_set, :all_entries
-    def_delegator :book_set, :all_entries, :ae
+  # Enable use of some BookSet methods in shell with long and short names aaa, ae:
 
-    def_delegator :book_set, :chart_of_accounts
-    def_delegator :book_set, :chart_of_accounts, :chart
+  def_delegator :book_set, :all_acct_amounts
+  def_delegator :book_set, :all_acct_amounts, :aaa
 
-    # For conveniently finding the project on Github from the shell
-    PROJECT_URL = 'https://github.com/keithrbennett/rock_books'
+  def_delegator :book_set, :all_entries
+  def_delegator :book_set, :all_entries, :ae
 
-    # Help text to be used when requested by 'h' command, in case of unrecognized or nonexistent command, etc.
-    HELP_TEXT = "
+  def_delegator :book_set, :chart_of_accounts
+  def_delegator :book_set, :chart_of_accounts, :chart
+
+  # For conveniently finding the project on Github from the shell
+  PROJECT_URL = 'https://github.com/keithrbennett/rock_books'
+
+  # Help text to be used when requested by 'h' command, in case of unrecognized or nonexistent command, etc.
+  HELP_TEXT = "
 Command Line Switches:                    [rock-books version #{RockBooks::VERSION} at https://github.com/keithrbennett/rock_books]
 
 -i   input directory specification, default: '.' (current directory)
@@ -46,7 +46,7 @@ Command Line Switches:                    [rock-books version #{RockBooks::VERSI
 
 Commands:
 
-rec[eipts]                - receipts: l/:l list, m/:m report missing receipts
+rec[eipts]                - receipts: a/:a all, m/:m missing, e/:e existing
 rep[orts]                 - return an OpenStruct containing all reports (interactive shell mode only)
 d[isplay_reports]         - display all reports on stdout
 w[rite_reports]           - write all reports to the output directory (see -o option)
@@ -58,283 +58,300 @@ q[uit]                    - exits this program (interactive shell mode only) (se
 x[it]                     - exits this program (interactive shell mode only) (see also 'q')
 
 When in interactive shell mode:
-  * use quotes for string parameters such as method names.
-  * for pry commands, use prefix `%`.
-  * you can use the global variable $filter to filter reports
+* use quotes for string parameters such as method names.
+* for pry commands, use prefix `%`.
+* you can use the global variable $filter to filter reports
 
 "
 
-    def initialize(run_options)
-      @run_options = run_options
-      @interactive_mode = !!(run_options.interactive_mode)
-      @verbose_mode = run_options.verbose
-      # book_set is set with a lazy initializer
+  def initialize(run_options)
+    @run_options = run_options
+    @interactive_mode = !!(run_options.interactive_mode)
+    @verbose_mode = run_options.verbose
+    # book_set is set with a lazy initializer
+  end
+
+
+  def print_help
+    puts HELP_TEXT
+  end
+
+
+  def enclose_in_hyphen_lines(string)
+    hyphen_line = "#{'-' * 80}\n"
+    hyphen_line + string + "\n" + hyphen_line
+  end
+
+
+  def receipt_full_filespec(receipt_filespec)
+    File.join(run_options.receipts_dir, receipt_filespec)
+  end
+
+
+  # Pry will output the content of the method from which it was called.
+  # This small method exists solely to reduce the amount of pry's output
+  # that is not needed here.
+  def run_pry
+    binding.pry
+
+    # the seemingly useless line below is needed to avoid pry's exiting
+    # (see https://github.com/deivid-rodriguez/pry-byebug/issues/45)
+    _a = nil
+  end
+
+
+  # Runs a pry session in the context of this object.
+  # Commands and options specified on the command line can also be specified in the shell.
+  def run_shell
+    begin
+      require 'pry'
+    rescue LoadError
+      message = "The 'pry' gem and/or one of its prerequisites, required for running the shell, was not found." +
+          " Please `gem install pry` or, if necessary, `sudo gem install pry`."
+      raise Error.new(message)
     end
 
+    print_help
 
-    def print_help
-      puts HELP_TEXT
-    end
+    # Enable the line below if you have any problems with pry configuration being loaded
+    # that is messing up this runtime use of pry:
+    # Pry.config.should_load_rc = false
 
+    # Strangely, this is the only thing I have found that successfully suppresses the
+    # code context output, which is not useful here. Anyway, this will differentiate
+    # a pry command from a DSL command, which _is_ useful here.
+    Pry.config.command_prefix = '%'
 
-    def enclose_in_hyphen_lines(string)
-      hyphen_line = "#{'-' * 80}\n"
-      hyphen_line + string + "\n" + hyphen_line
-    end
-
-
-    def receipt_full_filespec(receipt_filespec)
-      File.join(run_options.receipts_dir, receipt_filespec)
-    end
+    run_pry
+  end
 
 
-    # Pry will output the content of the method from which it was called.
-    # This small method exists solely to reduce the amount of pry's output
-    # that is not needed here.
-    def run_pry
-      binding.pry
+  # Look up the command name and, if found, run it. If not, execute the passed block.
+  def attempt_command_action(command, *args, &error_handler_block)
+    no_command_specified = command.nil?
+    command = 'help' if no_command_specified
 
-      # the seemingly useless line below is needed to avoid pry's exiting
-      # (see https://github.com/deivid-rodriguez/pry-byebug/issues/45)
-      _a = nil
-    end
+    action = find_command_action(command)
+    result = nil
 
-
-    # Runs a pry session in the context of this object.
-    # Commands and options specified on the command line can also be specified in the shell.
-    def run_shell
-      begin
-        require 'pry'
-      rescue LoadError
-        message = "The 'pry' gem and/or one of its prerequisites, required for running the shell, was not found." +
-            " Please `gem install pry` or, if necessary, `sudo gem install pry`."
-        raise Error.new(message)
-      end
-
-      print_help
-
-      # Enable the line below if you have any problems with pry configuration being loaded
-      # that is messing up this runtime use of pry:
-      # Pry.config.should_load_rc = false
-
-      # Strangely, this is the only thing I have found that successfully suppresses the
-      # code context output, which is not useful here. Anyway, this will differentiate
-      # a pry command from a DSL command, which _is_ useful here.
-      Pry.config.command_prefix = '%'
-
-      run_pry
-    end
-
-
-    # Look up the command name and, if found, run it. If not, execute the passed block.
-    def attempt_command_action(command, *args, &error_handler_block)
-      no_command_specified = command.nil?
-      command = 'help' if no_command_specified
-
-      action = find_command_action(command)
-      result = nil
-
-      if action
-        result = action.(*args)
-      else
-        error_handler_block.call
-        nil
-      end
-
-      if no_command_specified
-        puts enclose_in_hyphen_lines('! No operations specified !')
-      end
-      result
-    end
-
-
-    # For use by the shell when the user types the DSL commands
-    def method_missing(method_name, *method_args)
-      attempt_command_action(method_name.to_s, *method_args) do
-        puts(%Q{"#{method_name}" is not a valid command or option. } \
-          << 'If you intend for this to be a string literal, ' \
-          << 'use quotes or %q{}/%Q{}.')
-      end
-    end
-
-
-    # Processes the command (ARGV[0]) and any relevant options (ARGV[1..-1]).
-    #
-    # CAUTION! In interactive mode, any strings entered (e.g. a network name) MUST
-    # be in a form that the Ruby interpreter will recognize as a string,
-    # i.e. single or double quotes, %q, %Q, etc.
-    # Otherwise it will assume it's a method name and pass it to method_missing!
-    def process_command_line
-      attempt_command_action(ARGV[0], *ARGV[1..-1]) do
-        print_help
-        raise BadCommandError.new(
-            %Q{! Unrecognized command. Command was #{ARGV.first.inspect} and options were #{ARGV[1..-1].inspect}.})
-      end
-    end
-
-
-    def quit
-      if interactive_mode
-        exit(0)
-      else
-        puts "This command can only be run in shell mode."
-      end
-    end
-
-
-    def cmd_c
-      puts chart_of_accounts.report_string
-    end
-
-
-    def cmd_h
-      print_help
-    end
-
-
-    def cmd_j
-      journal_names = book_set.journals.map(&:short_name)
-      if interactive_mode
-        journal_names
-      else
-        ap journal_names
-      end
-    end
-
-
-    def book_set
-      @book_set ||= load_data
-    end
-
-
-    def load_data
-      @book_set = BookSetLoader.load(run_options.input_dir)
-    end
-    alias_method :reload_data, :load_data
-
-
-    def cmd_rel
-      reload_data
+    if action
+      result = action.(*args)
+    else
+      error_handler_block.call
       nil
     end
 
-
-    # All reports as Ruby objects; only makes sense in shell mode.
-    def cmd_rep
-      unless run_options.interactive_mode
-        raise Error.new("Option 'all_reports' is only available in shell mode. Try 'display_reports' or 'write_reports'.")
-      end
-
-      os = OpenStruct.new(book_set.all_reports($filter))
-      def os.keys; to_h.keys;     end  # add hash methods for convenience
-      def os.values; to_h.values; end
-      def os.at(index); self.public_send(keys[index]); end # to access as array, e.g. `a.at(1)`
-      os
+    if no_command_specified
+      puts enclose_in_hyphen_lines('! No operations specified !')
     end
+    result
+  end
 
 
-    def cmd_d
-      book_set.all_reports($filter).each do |short_name, report_text|
-        puts "#{short_name}:\n\n"
-        puts report_text
-        puts "\n\n\n"
-      end
-    end
-
-
-    def cmd_proj
-      `open https://github.com/keithrbennett/rock_books`
-    end
-
-
-    def cmd_rec(options)
-      case options.first.to_s
-      when 'l'
-        receipts = all_entries.map(&:receipts).flatten.sort
-        run_options.interactive_mode ? receipts : ap(receipts)
-      when 'm'
-        missing_receipts = all_entries.each_with_object([]) do |entry, missing_receipts|
-          entry.receipts.each do |receipt|
-            unless File.file?(receipt_full_filespec(receipt))
-              missing_receipts << { journal: entry.doc_short_name, receipt: receipt }
-            end
-          end
-        end
-        run_options.interactive_mode ? missing_receipts : ap(missing_receipts)
-      end
-    end
-
-    def cmd_w
-      book_set.all_reports_to_files(run_options.output_dir, $filter)
-    end
-
-
-    def cmd_x
-      quit
-    end
-
-
-    def commands
-      @commands_ ||= [
-          Command.new('rec', 'receipts',          -> (*options)  { cmd_rec(options)  }),
-          Command.new('rep', 'reports',           -> (*_options) { cmd_rep           }),
-          Command.new('d',   'display_reports',   -> (*_options) { cmd_d             }),
-          Command.new('w',   'write_reports',     -> (*_options) { cmd_w             }),
-          Command.new('c',   'chart_of_accounts', -> (*_options) { cmd_c             }),
-          Command.new('jo',  'journals',          -> (*_options) { cmd_j             }),
-          Command.new('h',   'help',              -> (*_options) { cmd_h             }),
-          Command.new('proj','project_page',      -> (*_options) { cmd_proj          }),
-          Command.new('q',   'quit',              -> (*_options) { cmd_x             }),
-          Command.new('rel', 'reload_data',       -> (*_options) { cmd_rel           }),
-          Command.new('x',   'xit',               -> (*_options) { cmd_x             })
-      ]
-    end
-
-
-    def find_command_action(command_string)
-      result = commands.detect do |cmd|
-        cmd.max_string.start_with?(command_string) \
-      && \
-      command_string.length >= cmd.min_string.length  # e.g. 'c' by itself should not work
-      end
-      result ? result.action : nil
-    end
-
-
-    # If a post-processor has been configured (e.g. YAML or JSON), use it.
-    def post_process(object)
-      post_processor ? post_processor.(object) : object
-    end
-
-
-    def post_processor
-      run_options.post_processor
-    end
-
-
-    # Convenience Method(s)
-
-    # Easier than remembering and typing Date.iso8601.
-    def td(date_string)
-      Date.iso8601(date_string)
-    end
-
-
-    def call
-      begin
-        # By this time, the Main class has removed the command line options, and all that is left
-        # in ARGV is the commands and their options.
-        if @interactive_mode
-          run_shell
-        else
-          process_command_line
-        end
-
-      rescue BadCommandError => error
-        separator_line = "! #{'-' * 75} !\n"
-        puts '' << separator_line << error.to_s << "\n" << separator_line
-        exit(-1)
-      end
+  # For use by the shell when the user types the DSL commands
+  def method_missing(method_name, *method_args)
+    attempt_command_action(method_name.to_s, *method_args) do
+      puts(%Q{"#{method_name}" is not a valid command or option. } \
+        << 'If you intend for this to be a string literal, ' \
+        << 'use quotes or %q{}/%Q{}.')
     end
   end
+
+
+  # Processes the command (ARGV[0]) and any relevant options (ARGV[1..-1]).
+  #
+  # CAUTION! In interactive mode, any strings entered (e.g. a network name) MUST
+  # be in a form that the Ruby interpreter will recognize as a string,
+  # i.e. single or double quotes, %q, %Q, etc.
+  # Otherwise it will assume it's a method name and pass it to method_missing!
+  def process_command_line
+    attempt_command_action(ARGV[0], *ARGV[1..-1]) do
+      print_help
+      raise BadCommandError.new(
+          %Q{! Unrecognized command. Command was #{ARGV.first.inspect} and options were #{ARGV[1..-1].inspect}.})
+    end
+  end
+
+
+  def quit
+    if interactive_mode
+      exit(0)
+    else
+      puts "This command can only be run in shell mode."
+    end
+  end
+
+
+  def cmd_c
+    puts chart_of_accounts.report_string
+  end
+
+
+  def cmd_h
+    print_help
+  end
+
+
+  def cmd_j
+    journal_names = book_set.journals.map(&:short_name)
+    if interactive_mode
+      journal_names
+    else
+      ap journal_names
+    end
+  end
+
+
+  def book_set
+    @book_set ||= load_data
+  end
+
+
+  def load_data
+    @book_set = BookSetLoader.load(run_options.input_dir)
+  end
+  alias_method :reload_data, :load_data
+
+
+  def cmd_rel
+    reload_data
+    nil
+  end
+
+
+  # All reports as Ruby objects; only makes sense in shell mode.
+  def cmd_rep
+    unless run_options.interactive_mode
+      raise Error.new("Option 'all_reports' is only available in shell mode. Try 'display_reports' or 'write_reports'.")
+    end
+
+    os = OpenStruct.new(book_set.all_reports($filter))
+    def os.keys; to_h.keys;     end  # add hash methods for convenience
+    def os.values; to_h.values; end
+    def os.at(index); self.public_send(keys[index]); end # to access as array, e.g. `a.at(1)`
+    os
+  end
+
+
+  def cmd_d
+    book_set.all_reports($filter).each do |short_name, report_text|
+      puts "#{short_name}:\n\n"
+      puts report_text
+      puts "\n\n\n"
+    end
+  end
+
+
+  def cmd_proj
+    `open https://github.com/keithrbennett/rock_books`
+  end
+
+
+  def missing_and_existing_receipts
+    missing = []; existing = []
+    all_entries.each do |entry|
+      entry.receipts.each do |receipt|
+        file_exists = File.file?(receipt_full_filespec(receipt))
+        list = (file_exists ? existing : missing)
+        list << receipt
+      end
+    end
+    [missing, existing]
+  end
+
+  def cmd_rec(options)
+    missing, existing = missing_and_existing_receipts
+
+    print_missing  = -> { puts "Missing Receipts:"; ap missing }
+    print_existing = -> { puts "Existing Receipts:"; ap existing }
+
+    case options.first.to_s
+      when 'a'  # all
+        if run_options.interactive_mode
+          [missing, existing]
+        else
+           print_missing.(); print_existing.()
+        end
+
+      when 'm'
+        run_options.interactive_mode ? missing : print_missing.()
+
+      when 'e'
+        run_options.interactive_mode ? existing : print_existing.()
+    end
+  end
+
+  def cmd_w
+    book_set.all_reports_to_files(run_options.output_dir, $filter)
+  end
+
+
+  def cmd_x
+    quit
+  end
+
+
+  def commands
+    @commands_ ||= [
+        Command.new('rec', 'receipts',          -> (*options)  { cmd_rec(options)  }),
+        Command.new('rep', 'reports',           -> (*_options) { cmd_rep           }),
+        Command.new('d',   'display_reports',   -> (*_options) { cmd_d             }),
+        Command.new('w',   'write_reports',     -> (*_options) { cmd_w             }),
+        Command.new('c',   'chart_of_accounts', -> (*_options) { cmd_c             }),
+        Command.new('jo',  'journals',          -> (*_options) { cmd_j             }),
+        Command.new('h',   'help',              -> (*_options) { cmd_h             }),
+        Command.new('proj','project_page',      -> (*_options) { cmd_proj          }),
+        Command.new('q',   'quit',              -> (*_options) { cmd_x             }),
+        Command.new('rel', 'reload_data',       -> (*_options) { cmd_rel           }),
+        Command.new('x',   'xit',               -> (*_options) { cmd_x             })
+    ]
+  end
+
+
+  def find_command_action(command_string)
+    result = commands.detect do |cmd|
+      cmd.max_string.start_with?(command_string) \
+    && \
+    command_string.length >= cmd.min_string.length  # e.g. 'c' by itself should not work
+    end
+    result ? result.action : nil
+  end
+
+
+  # If a post-processor has been configured (e.g. YAML or JSON), use it.
+  def post_process(object)
+    post_processor ? post_processor.(object) : object
+  end
+
+
+  def post_processor
+    run_options.post_processor
+  end
+
+
+  # Convenience Method(s)
+
+  # Easier than remembering and typing Date.iso8601.
+  def td(date_string)
+    Date.iso8601(date_string)
+  end
+
+
+  def call
+    begin
+      # By this time, the Main class has removed the command line options, and all that is left
+      # in ARGV is the commands and their options.
+      if @interactive_mode
+        run_shell
+      else
+        process_command_line
+      end
+
+    rescue BadCommandError => error
+      separator_line = "! #{'-' * 75} !\n"
+      puts '' << separator_line << error.to_s << "\n" << separator_line
+      exit(-1)
+    end
+  end
+end
 end
