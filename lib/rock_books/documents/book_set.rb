f6ac62e6ced98500f261a3b9d154336f8a729b0a
Keith Bennett <keithrbennett@gmail.com>
2019-01-09 23:12:57 +0700

    Change HTML font to Courier New Bold for readable output on both Mac & Windows.

--- lib/rock_books/documents/book_set.rb
+++ lib/rock_books/documents/book_set.rb
@@ -105,7 +105,7 @@ module RockBooks
           pdf_filespec  = build_filespec.(directory, short_name, 'pdf')
 
           File.write(txt_filespec, report_text)
-          run_command("textutil -convert html -font 'Menlo Regular' -fontsize 12 #{txt_filespec} -output #{html_filespec}")
+          run_command("textutil -convert html -font 'Courier New Bold' -fontsize 12 #{txt_filespec} -output #{html_filespec}")
           run_command("cupsfilter #{html_filespec} > #{pdf_filespec}")
           puts "Created reports in txt, html, and pdf for #{"%-20s" % short_name} at #{File.dirname(txt_filespec)}.\n\n\n"
         end
