0ba03b17c9e5f0cddac1beb968e4479c5d9312bb
Keith Bennett <keithrbennett@gmail.com>
2020-12-25 14:12:25 -0500

    Add validation of code parameter in Account class.

--- lib/rock_books/types/account.rb
+++ lib/rock_books/types/account.rb
@@ -2,6 +2,18 @@ module RockBooks
 
   # The ChartOfAccount holds the set of all these accounts for the entity.
   class Account < Struct.new(:code, :type, :name)
-  end
 
+    PERMITTED_CODE_CHAR_REGEX = /[\w\-\.]$/ # word chars (letters, numbers, underscores), hyphens, periods
+
+    def validate_code(code)
+      unless PERMITTED_CODE_CHAR_REGEX.match(code)
+        raise "Code {#{code}} may only contain letters, numbers, underscores, hyphens, and periods."
+      end
+    end
+
+    def initialize(code, type, name)
+      validate_code(code)
+      super
+    end
+  end
 end
