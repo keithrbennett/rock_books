a1380b1482d8f3a08956b1b4e89b80f20cc3ac56
Keith Bennett <keithrbennett@gmail.com>
2021-01-09 20:42:59 -0500

    Refactor method with lambdas into a private class.

--- lib/rock_books/reports/helpers/html_report_helper.rb
+++ lib/rock_books/reports/helpers/html_report_helper.rb
@@ -1,35 +1,47 @@
 module RockBooks
-module HtmlReportHelper
+class ReceiptsHyperlinkConverter
 
-  def self.convert_receipts_to_hyperlinks(original_html_string, html_filespec)
-    html_lines = original_html_string.split("\n")
-    content_changed = false
+  def self.convert_receipts_to_hyperlinks(html_string, html_filespec)
+    ReceiptsHyperlinkConverter.new(html_string, html_filespec).convert
+  end
 
-    # If the HTML file being created is in DATA_DIR/rockbooks-reports/html/single-account, then
-    #   the processed link should be '../../../receipts/[receipt_filespec]'
-    # else it's in DATA_DIR/rockbooks-reports/html, and
-    #   the processed link should be '../../receipts/[receipt_filespec]'
-    processed_receipt_filespec = ->(listed_receipt_filespec) do
-      num_dirs_up = html_filespec.include?('/single-account/') ? 3 : 2
-      File.join(('../' * num_dirs_up), 'receipts', listed_receipt_filespec)
-    end
+  RECEIPT_REGEX = /Receipt:\s*(\S*)/
 
-    receipt_anchor_line = ->(line, listed_receipt_filespec) do
-      line.gsub( \
-            /Receipt:\s*#{listed_receipt_filespec}/, \
-            %Q{Receipt: <a href="#{processed_receipt_filespec.(listed_receipt_filespec)}">#{listed_receipt_filespec}</a>})
-    end
+  attr_reader :html_string, :num_dirs_up
 
-    html_lines.each_with_index do |line, index|
-      matches = /Receipt:\s*(\S*)/.match(line)
+  def initialize(html_string, html_filespec)
+    @html_string = html_string
+    @num_dirs_up = html_filespec.include?('/single-account/') ? 3 : 2
+  end
+
+
+  def convert
+    html_string.split("\n").map do |line|
+      matches = RECEIPT_REGEX.match(line)
       if matches
         listed_receipt_filespec = matches[1]
-        html_lines[index] = receipt_anchor_line.(line, listed_receipt_filespec)
-        content_changed = true
+        receipt_anchor_line(line, listed_receipt_filespec)
+      else
+        line
       end
-    end
-
-    content_changed ? html_lines.join("\n") : original_html_string
+    end.join("\n")
   end
+
+
+  # If the HTML file being created is in DATA_DIR/rockbooks-reports/html/single-account, then
+  #   the processed link should be '../../../receipts/[receipt_filespec]'
+  # else it's in DATA_DIR/rockbooks-reports/html, and
+  #   the processed link should be '../../receipts/[receipt_filespec]'
+  private def processed_receipt_filespec(listed_receipt_filespec)
+    File.join(('../' * num_dirs_up), 'receipts', listed_receipt_filespec)
+  end
+
+  private def receipt_anchor_line(line, listed_receipt_filespec)
+    line.gsub( \
+          /Receipt:\s*#{listed_receipt_filespec}/, \
+          %Q{Receipt: <a href="#{processed_receipt_filespec(listed_receipt_filespec)}">#{listed_receipt_filespec}</a>})
+  end
+
 end
 end
+
