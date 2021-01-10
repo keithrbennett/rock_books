63d9c671ce51a977b022db5b0618a474d5175dd6
Keith Bennett <keithrbennett@gmail.com>
2021-01-09 21:07:53 -0500

    Add report generation timestamp to all reports.

--- lib/rock_books/reports/helpers/text_report_helper.rb
+++ lib/rock_books/reports/helpers/text_report_helper.rb
@@ -130,7 +130,7 @@ module TextReportHelper
       fn_format_multidoc_entry: method(:format_multidoc_entry),
       fn_section_heading: method(:section_heading),
       fn_total_with_ok_or_discrepancy: method(:total_with_ok_or_discrepancy),
-      generated: "Reports Generated at #{DateTime.now.strftime('%Y-%m-%d %H:%M:%S')} by RockBooks version #{RockBooks::VERSION}",
+      generated: "Generated at #{DateTime.now.strftime('%Y-%m-%d %H:%M:%S')} by RockBooks version #{RockBooks::VERSION}",
       line_item_format_string: line_item_format_string,
       short_name_format_string: SHORT_NAME_FORMAT_STRING,
       start_date: start_date,
